828475faf0d84014648065e8d61975584a47076e
gdmec07150710 <1104094926@qq.com>
2016-12-11 23:02:41 +0800

    Initial commit

---
+++ app/src/main/java/com/gdmec07150710/viewfilpperdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gdmec07150710.viewfilpperdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
